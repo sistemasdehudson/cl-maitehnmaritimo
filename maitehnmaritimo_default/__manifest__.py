77ac556a5cd19dfbbc90eff011f9eb8d8124679d
concagabriel <58643247+concagabriel@users.noreply.github.com>
2021-07-08 10:08:45 -0300

    Update __manifest__.py

--- maitehnmaritimo_default/__manifest__.py
+++ maitehnmaritimo_default/__manifest__.py
@@ -66,6 +66,7 @@
         'https://github.com/OCA/pos.git',
         ##'https://github.com/oca/report-print-send/tree/13.0',
         'https://github.com/OCA/report-print-send.git',
+        'https://github.com/pronexo-argentina/pos_proxy_services.git -b 13.0',
     ],
 
     'docker-images': [
